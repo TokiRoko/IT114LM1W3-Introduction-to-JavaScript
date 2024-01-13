5687bc5490bebd628ccb6389b9308a07d784fb83
TokiRoko <154581531+TokiRoko@users.noreply.github.com>
2024-01-13 10:13:40 +0000

    First Commit

--- 1. Variables, Data Types, and Operators/index.js
+++ 1. Variables, Data Types, and Operators/index.js
@@ -5,43 +5,128 @@
 
 // TODO 1.1 Use the var keyword to define a variable. Log the variable to the console. Now try moving the variable declaration after you log the variable.
 // Your code here
-
+console.log(name);
+var name = "Namae-wa";
+console.log(name);
 // TODO 1.2 Use the let keyword to define a variable.
 // Your code here
+let name_ex = "Name";
 
+// if (name_ex === "Name"){
+//     name_ex = "Harold";
+//     console.log(name_ex);
+// }
 // TODO 1.3 Use the const keyword to define a variable. Try reassigning the value of the variable. What happens?
 // Your code here
+const number = 10;
 
+// number = 21; It will have an error since the variable has a const and it connot be reassigned unless if you use the let function replacing the const.
 // Checkpoint 1.1 What is the difference between var, let, and const?
-// Answer: 
+// Answer: the difference between the three statements is the functioning of the variables and the reassigning of the variables. They have their own block-scoped functionality with limited access.
 
 // TODO 1.4 Try out the different arithmetic operators
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#arithmetic_operators
 // Your code here
+// Assignment Operator
+let x = 2;
+const y = 10;
+
+// console.log(x);
+// Expected Output: 2
+
+// console.log(y);
+// Expected Output: 10
+
+// Addition Operator
+let a = "Washing";
+let b = "ton";
+let c = " Machine";
+
+// console.log(a+=c);
+// Expected Output: Washing Machine
+
+// Subtract Operator
+let num1 = 20;
+
+// console.log(num1 -= 9);
+// Expected Output: 11
+
+// Multiplication Operator
+let num2 = 4;
+
+// console.log(num1 *= num2);
+// Expected Output: 80
+
+//Division Operator
+let num3 = 2;
+
+// console.log(num1 /= num3);
+//Expected Output: 10
 
 // Checkpoint 1.2 What operators did you use?
-// Answer: 
+// Answer: I use the assignment, addition, subtraction, multiplication and division.
 // Your code here
+// console.log(a+=c); addition operator
+// let x = 2;
+// const y = 10; Assignment operators
+// console.log(num1 -= 9);
+// console.log(num1 *= num2);
+// console.log(num1 /= num3);
 
 // TODO 1.5 Try out the different string operators
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#string_operators
 // Your code here
 
+// console.log(a+=b);
+// Expected Output: "Washington"
+let washington= " D.C.";
+let fullName = a + b + washington;
+console.log(fullName);
+//Expected Output: Washington D.C.
+
+let greetings = "Good Afternoon friends,";
+let message = " I am now " + num1 + " years old.";
+console.log(greetings + message);
+//Expected Output: Good Afternoon friends, I am now 20 years old.
+
+
 // Checkpoint 1.3 What operators did you use?
-// Answer: 
+// Answer: I only use the addition operator to combine the words.
 
 // TODO 1.6 Try out the different logical operators
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#logical_operators
 // Your code here
+const a1 = true && true; // returns true
+const a2 = true && false; // returns false
+const a3 = false && false; // returns false
+const b1 = true || true; // returns true
+const b2 = true || false; // returns true
+const b3 = false || false; // returns false
+const c1 = !true && false; // returns false
+const c2 = !true && !false; // returns false
+const c3 = !false || true; // returns true
+const c4 = !true || true; // returns true
 
 // Checkpoint 1.4 What operators did you use?
-// Answer: 
+// Answer: AND, OR, and NOR operators
 
 
 // TODO 1.7 Try out the different relational operators
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#relational_operators
 
+const fruits = ["apple", "grapes", "orange", "peach", "blueberry"];
+
+0 in fruits; //returns true
+"apple" in fruits; // returns false
+
 
 // Checkpoint 1.5 What does [] == false evaluate to? Why does that happen? Research why
-// Answer: 
+// Answer: According to developer.mozilla.org, when comparing an array with a boolean using the loose equal sign,
+// it returns to true since we are comparing if the array and the boolean are, for example false, then the console will return a statement which is true.
+// but if the equal sign has 3 then it will return to false according to my observation.
 // Your code here
+
+const array = [];
+
+console.log(array == false); //returns true
+console.log(array === false); //returns false
--- 2. Control Structures/index.js
+++ 2. Control Structures/index.js
@@ -8,56 +8,116 @@ const randomNumber = Math.floor((Math.random() * 100) + 1); // feel free to mock
 // TODO 2.1 Display "more than fifty" if randomNumber is more than fify
 // Your code here
 
+if (randomNumber > 50){
+    console.log("more than fifty.");
+}
+else{
+    console.log("less than fifty.");
+}
+
 // TODO 2.2 Display whether the random number is odd or even
 // Your code here
+
+if (randomNumber % 2 === 0){
+    console.log(randomNumber + " It is an even number");
+}
+else{
+    console.log(randomNumber + " It is an odd number");
+}
 
 // TODO 2.3 If the number if a multiple of 3, write "fizz".
 // if the number is a multiple of 5 display "buzz".
 // if the number is divisible by both 3 and 5, display "fizzbuzz". otherwise, display the number
 // Your code here
+if (randomNumber % 3 === 0){
+    console.log("fizz");
+}
+else if (randomNumber % 5 === 0){
+    console.log("buzz");
+}
+else{
+    console.log("fizzbuzz");
+}
 
-let toDisplay = ""
+
+let toDisplay = randomNumber % 2 === 0 ? "Even":"Odd";
 // TODO 2.4 Use the ternary conditional operator to set toDisplay to "Even" if randomNumber is even and "Odd" if randomNumber is odd
 // Your code here
+console.log(randomNumber + " is " + toDisplay + ".");
+
+
 console.log("toDisplay", toDisplay);
 
 // Checkpoint 2.1 How do you use switch statements and when would you use them? Try researching the answer
-// Answer: 
+// Answer: A switch statement in JavaScript is used to perform different actions based on different conditions. 
+// It provides a way to express multiple branches of conditional logic in a more structured and readable format. 
+// You would use switch statements when you have a single expression that you want to evaluate 
+// against multiple possible values, and each value corresponds to a different set of actions or outcomes.
 
 // TODO 2.5 Use a for loop to print the numbers 1 to N
 const n = 10
 // Your code here
+for (let i=1; i <= n; i++){
+    console.log(i);
+}
 
 const list = ["apple", "banana", "cherry", "date", "elderberry"];
 // TODO 2.6 Use a while loop to display all the values in the list
 // Your code here
+let i = 0;
+
+while (i < list.length){
+    console.log(list[i]);
+    i++;
+}
 
 // Checkpoint 2.3 What is the difference between do while and while loop?
-// Answer: 
+// Answer: In a while loop, the loop condition is evaluated before the loop body is executed. 
+// If the condition is false initially, the loop body may not execute at all.
+// In a do-while loop, the loop body is executed at least once, and then the condition is evaluated. 
+// If the condition is true, the loop body will continue to execute; otherwise, the loop exits.
 
 // TODO 2.7 Use a for of loop to display all the values in the list
 // Your code here
+for (const value of list) {
+    console.log(value);
+}
 
 // TODO 2.8 Use a for in loop to display all the values in the list
 // Your code here
+for (const value in list) {
+    console.log(value);
+}
 
 // TODO 2.9 Use the for each method of the list to display all its values 
 // Your code here
+list.forEach(function(value) {
+    console.log(value);
+});
 
 // Checkpoint 2.2 When should you use for of, for in, or .forEach loops? Try researching the answer
-// Answer: 
+// Answer: Use the for...of loop when iterating over values in iterables, for...in when traversing enumerable properties of objects, 
+// and the forEach method for a concise way to iterate through elements of arrays.
 
 // TODO 2.10 Use the try and catch block to catch division by zero errors in the code below.
 // In the finally block, simulate cleaning up resources by displaying "cleaning up resources"
 const numerator = Math.floor((Math.random() * 100) + 1);
 const denominator = Math.floor((Math.random() * 5)); // feel free to mock this value for testing
 
-
-if(denominator === 0){
-    throw new Error("Division by zero error")
-} else {
-    console.log(numerator / denominator);
+try {
+    if(denominator === 0){
+        throw new Error("Division by zero error")
+    } else {
+        console.log(numerator / denominator);
+    }
 }
+catch (error){
+    console.log("Error:", error.message);
+}
+finally{
+    console.log("Cleaning up resources");
+}
+
 
 
 
